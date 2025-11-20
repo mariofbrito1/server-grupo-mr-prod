82772e2fea0f6be58ddf73cf71745b4f07bbddf8
m@rio <mariofbrito@gmail.com>
2025-11-19 23:05:14 -0300

    update

--- test-babel.js
+++
@@ -1,7 +0,0 @@
-// src/test-babel.js
-console.log('✅ Babel test - Si ves esto, Babel funciona');
-console.log('Node version:', process.version);
-
-// Test de sintaxis ES6
-const test = { message: 'Babel funciona' };
-console.log('Test object:', test);
