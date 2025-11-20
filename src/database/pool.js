a66c9e3fd45923d45ef7b1716d2edbe4fb4685b3
m@rio <mariofbrito@gmail.com>
2025-11-19 23:20:16 -0300

    mario ver

--- src/database/pool.js
+++ src/database/pool.js
@@ -1,5 +1,5 @@
 const { Pool } = require('pg');
-const config = require('./../src/config'); 
+const config = require('../config'); 
 
 // Pool global compartido para toda la aplicación
 const pool = new Pool({
